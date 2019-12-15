f34e19d5c1c9309f82ec95b24716a210f5d17ba9
Orion Wolf-Hubbard <orion@Orions-MacBook-Pro.local>
2019-12-14 23:54:53 -0500

    deply

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,5 +65,5 @@ const config: SocketIoConfig = { url: 'https://retroserver.herokuapp.com/', opti
 })
 export class AppModule { }
 
-// ng build --prod --output-path docs --base-href retro-client
+// ng build --prod --output-path docs --base-href /retro-client/
 // https://owolfhu1.github.io/retro-client
